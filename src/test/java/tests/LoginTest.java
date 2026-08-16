2d227147792e8f59f53acb45a9cd139d81575a55
Suraj Gope <yaduvanshisuraj809@gmail.com>
2026-08-16 12:37:41 +0530

    Refactor LoginTest using BaseTest

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,40 +1,21 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import base.BaseTest;
 import pages.LoginPage;
 
-public class LoginTest {
-
-    WebDriver driver;
-    LoginPage loginPage;
-
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        loginPage = new LoginPage(driver);
-    }
+public class LoginTest extends BaseTest {
 
     @Test
     public void verifySalesforceLoginPage() {
 
         driver.get("https://login.salesforce.com");
 
+        LoginPage loginPage = new LoginPage(driver);
+
         loginPage.enterUsername("test@example.com");
         loginPage.enterPassword("TestPassword123");
         loginPage.clickLogin();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
     }
 }

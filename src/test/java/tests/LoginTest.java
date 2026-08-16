0e53b99409d17485a57d74e3eebbf2396489f460
Suraj Gope <yaduvanshisuraj809@gmail.com>
2026-08-16 11:58:27 +0530

    Connect LoginTest with Page Object Model

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -6,24 +6,29 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import pages.LoginPage;
+
 public class LoginTest {
 
     WebDriver driver;
+    LoginPage loginPage;
 
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+
+        loginPage = new LoginPage(driver);
     }
 
     @Test
-    public void verifyLoginPage() {
+    public void verifySalesforceLoginPage() {
 
         driver.get("https://login.salesforce.com");
 
-        String actualTitle = driver.getTitle();
-
-        System.out.println("Page Title: " + actualTitle);
+        loginPage.enterUsername("test@example.com");
+        loginPage.enterPassword("TestPassword123");
+        loginPage.clickLogin();
     }
 
     @AfterMethod
